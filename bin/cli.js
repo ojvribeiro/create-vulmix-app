c18a95610d1758ac4dbd0c2c258b65d32f631c1c
Victor Ribeiro <devjvribeiro@gmail.com>
2022-10-04 05:05:45 -0300

    Changed the starter template repo

--- bin/cli.js
+++ bin/cli.js
@@ -25,7 +25,7 @@ if (process.argv.length < 3) {
 const projectName = process.argv[2]
 const currentPath = process.cwd()
 const projectPath = path.join(currentPath, projectName)
-const git_repo = 'https://github.com/ojvribeiro/vulmix.git'
+const git_repo = 'https://github.com/ojvribeiro/vulmix-starter-template.git'
 
 try {
   fs.mkdirSync(projectPath)
